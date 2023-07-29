650eb148d4d4ae2d9c5ecaf47264f72f77ebe5fa
ashikur37 <piash3700@gmail.com>
2023-07-29 11:49:21 +0600

    test kv in live

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,20 +1,23 @@
 export const fetchHomeData = async () => {
+
   const [sliderRes, topCategoriesRes, bestSaleProductsRes] = await Promise.all([
     fetch(process.env.API_ENDPOINT + "sliders"),
     fetch(process.env.API_ENDPOINT + "categories/trending?slice=false"),
-    fetch(process.env.API_ENDPOINT + "products/section/best_selling_home", {
-      // @ts-ignore
-      cf: {
-        cacheTtl: 1800,
-        cacheEverything: true,
-      },
-    }),
+    fetch(process.env.API_ENDPOINT + "products/section/best_selling_home"),
   ]);
   const [sliders, topCategories, bestSaleProducts] = await Promise.all([
     sliderRes.json(),
     topCategoriesRes.json(),
     bestSaleProductsRes.json(),
   ]);
+  //@ts-ignore
+  const { vv } = (process.env as { vv });
+  await vv.put('home_data', {
+    sliders: sliders.data,
+    topCategories: topCategories.data,
+    bestProducts: bestSaleProducts.data,
+  });
+  
   return {
     sliders: sliders.data,
     topCategories: topCategories.data,

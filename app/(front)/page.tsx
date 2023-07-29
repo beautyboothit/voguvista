563c76311a2a360f59314177b2e13ed81d1adaf0
ashikur37 <piash3700@gmail.com>
2023-07-29 12:41:43 +0600

    test kv in live

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,5 +1,6 @@
 export const runtime = "edge";
 export const revalidate = 1800;
+import { KVNamespace } from "@cloudflare/workers-types";
 import { fetchHomeData } from "@/lib/fetcher";
 import Banner from "../components/home/Banner";
 import BestSale from "../components/home/BestSale";
@@ -10,9 +11,16 @@ import TopProducts from "../components/home/TopProducts";
 import TrendingProducts from "../components/home/TrendingProducts";
 
 export default async function Home() {
+  let res:any="empty";
+  const { vv } = (process.env as unknown as { vv:KVNamespace });
+  if(vv){
+    res = await vv.get("home_data");
+  }
+  
   const { sliders, topCategories, bestProducts } = await fetchHomeData();
   return (
     <div>
+      {JSON.stringify(res)}
       <Banner sliders={sliders} />
 
       <div className="container mx-auto">

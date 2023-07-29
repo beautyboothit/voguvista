8fdb02e64f2e437c943f825ea4db90756625353d
ashikur37 <piash3700@gmail.com>
2023-07-29 18:15:29 +0600

    test kv in live

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,4 +1,4 @@
-// import { KVNamespace } from "@cloudflare/workers-types";
+import { KVNamespace } from "@cloudflare/workers-types";
 
 export const fetchHomeData = async () => {
 
@@ -12,12 +12,12 @@ export const fetchHomeData = async () => {
     topCategoriesRes.json(),
     bestSaleProductsRes.json(),
   ]);
-  // const { test } = (process.env as unknown as { test:KVNamespace });
-  //   //  await test.put("home_data",JSON.stringify({
-  //   //   sliders: sliders.data,
-  //   //   topCategories: topCategories.data,
-  //   //   bestProducts: bestSaleProducts.data,
-  //   // }));
+  const { kv } = (process.env as unknown as { kv:KVNamespace });
+     await kv.put("home_data",JSON.stringify({
+      sliders: sliders.data,
+      topCategories: topCategories.data,
+      bestProducts: bestSaleProducts.data,
+    }));
   // const homeData=await test.get('home_data');
   // return JSON.parse(homeData!);
   return {

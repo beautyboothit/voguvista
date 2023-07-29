130b081b01a7b8d52c437a71d13d1bb37dd7b801
ashikur37 <piash3700@gmail.com>
2023-07-29 18:52:56 +0600

    KV  in live

--- app/(front)/categories/[slug]/page.tsx
+++ app/(front)/categories/[slug]/page.tsx
@@ -1,10 +1,13 @@
 export const runtime="edge"
+import { KVNamespace } from "@cloudflare/workers-types";
 type Props = {
     params: {
         slug: string
     }
 }
-export default function CategopryPage({params}:Props){
+export default async function CategopryPage({params}:Props){
+    const { data } = (process.env as unknown as { data:KVNamespace });
+    const homeData=await data.get('home_data');
     return (
         <div>
             <h1>{params.slug}</h1>
--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,6 +1,5 @@
 export const runtime = "edge";
 export const revalidate = 1800;
-import { KVNamespace } from "@cloudflare/workers-types";
 import { fetchHomeData } from "@/lib/fetcher";
 import Banner from "../components/home/Banner";
 import BestSale from "../components/home/BestSale";
@@ -12,8 +11,7 @@ import TrendingProducts from "../components/home/TrendingProducts";
 
 export default async function Home() {
   const { sliders, topCategories, bestProducts } = await fetchHomeData();
-  const { test } = (process.env as unknown as { test:KVNamespace });
-  // const homeData=await test.get('home_data');
+  
   return (
     <div>
       <Banner sliders={sliders} />
